70c82fedd898449b7dd9401a1d6423e763c12b19
Manuel <manumaxerapro@gmail.com>
2022-05-11 23:39:13 -0300

    ej2

--- myApp.js
+++ myApp.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
