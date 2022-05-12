66a64be082c6404b3118a95ee58d51fc145f4af4
Manuel <manumaxerapro@gmail.com>
2022-05-12 19:49:51 -0300

    Finalizacion mongoose

--- myApp.js
+++ myApp.js
@@ -22,48 +22,97 @@ const createAndSavePerson = (done) => {
   })
 }
 
+//Create Many Records with model.create()
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+]
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function(err, data){
+    if (err) return console.error(err)
+    done(null, data);  
+  })
 };
 
+//Use model.find() to Search Your Database
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
+  Person.find({name: personName}, function(err, data){
+    if (err) return console.error(err)
+    done(null, data)
+  })
+}
 
+//Use model.findOne() to Return a Single Matching Document from Your Database
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function(err, data){
+    if (err) return console.log(err)
+    done(null, data);    
+  })
 };
 
+//Use model.findById() to Search Your Database By _id
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, data){
+    if (err) return console.log(err)
+    done(null, data);
+  })
 };
 
+//Perform Classic Updates by Running Find, Edit, then Save
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  
+  Person.findById(personId, function(err, person){
+    if (err) return console.log(err)
+    
+    person.favoriteFoods.push(foodToAdd)
+  
+    person.save(function(err, updatedPerson){
+    if (err) return console.error(err)
+    done(null, updatedPerson)
+    })
+  })
 };
 
+//Perform New Updates on a Document Using model.findOneAndUpdate()
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, function(err, person){
+    if (err) return console.log(err)
+    done(null, person)
+  })
+}
 
-  done(null /*, data*/);
-};
-
+//Delete One Document Using model.findByIdAndRemove
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function(err, person){
+    if(err) return console.log(err)
+    done(null, person)
+  })
 };
 
+//Delete Many Documents with model.remove()
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, function(err, person){
+    if (err) console.log(err)
+    done(null, person);
+  })
 };
 
+//Chain Search Query Helpers to Narrow Search Results
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}) //realiza la busqueda de las personas que coincidan con la favoriterFoods = burrito
+        .sort({name: 1}) //ordena de manera descendente
+        .limit(2) //limita los documentos a 2
+        .select({age: 0}) //oculta el valor de edad, porque esta seteado a 0
+        .exec(function(err, data){ //le pasamos la callback a .exec ya que siempre se le pasa la callback funcion al ultimo metodo
+          if(err) console.log(err) //ya que si no hacemos esto la query no es ejecutada 
+          done(err, data)}) 
 };
 
 /** **Well Done !!**
